a53859b52e3813ca864213699c50234f07aa4aec
Valeria Gabrielli <gabvale53@gmail.com>
2026-01-20 15:05:30 +0100

    feat: jumbo css completed

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,9 @@
 
 import digitalComics from '../assets/buy-comics-digital-comics.png';
 import merchComics from '../assets/buy-comics-merchandise.png';
-import comicShop from '../assets/buy-comics-shop-locator.png'
+import comicSub from '../assets/buy-comics-subscriptions.png';
+import comicShop from '../assets/buy-comics-shop-locator.png';
+import buyVisa from '../assets/buy-dc-power-visa.svg';
 function MainContent() {
 
     return (
@@ -19,6 +21,19 @@ function MainContent() {
                         <img src={merchComics} alt="" />
                         <a href="#">DC MERCHANDISE</a>
                     </li>
+                       <li>
+                        <img src={comicSub} alt="" />
+                        <a href="#">SUBSCRIPTION</a>
+                    </li>
+                       <li>
+                        <img src={comicShop} alt="" />
+                        <a href="#">COMIC SHOP LOCATOR</a>
+                    </li>
+                       <li>
+                        <img src={buyVisa} alt="" />
+                        <a href="#">DC POWER VISA</a>
+                    </li>
+            
             
                 </ul>
             </div>
